b15f0e34ec87d511cf7f0ceede23d9e1de2a0808
brendanmcnally22 <brendanmcnally295@gmail.com>
2025-10-30 08:11:28 -0400

    Add Code - Test Commit / Merge

--- unity-project/mini-game-collection/Assets/2025/Team10/Scripts/TestScript.cs
+++ unity-project/mini-game-collection/Assets/2025/Team10/Scripts/TestScript.cs
@@ -7,6 +7,12 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Hey this is our first actual commit
+
+        // Hopefully this won't cause any problems, 
+
+        // I'm going to merge it into main
+
         
     }
 
